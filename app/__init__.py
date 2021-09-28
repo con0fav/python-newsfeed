aec777837cab716fca9db8dbf0ac1f70fee71362
con0fav <connor.favero@gmail.com>
2021-09-28 16:52:33 -0700

    building routes, testing

--- app/__init__.py
+++ app/__init__.py
@@ -9,5 +9,8 @@ def create_app(test_config=None):
   )
   @app.route('/hello')
   def hello():
-    return 'hello world' 
+    return 'hello world'
+    # register routes
+    app.register_blueprint(home)
+
   return app
--- app/routes/home.py
+++ app/routes/home.py
@@ -1,3 +1,5 @@
+from app.routes import home
+
 from flask import Blueprint, render_template
 
 bp = Blueprint('home', __name__, url_prefix='/')
@@ -9,3 +11,7 @@ def index():
 @bp.route('/login')
 def login():
   return render_template('login.html')
+
+@bp.route('/post/<id>')
+def single(id):
+  return render_template('single-post.html')
