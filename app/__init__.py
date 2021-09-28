cd30513a52399f6d949251f188b2e6393adb7676
con0fav <connor.favero@gmail.com>
2021-09-28 16:29:38 -0700

    added templates to app folder, hello world route in app

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
+from flask import Flask
+
 def create_app(test_config=None):
   # set up app config
   app = Flask(__name__, static_url_path='/')
@@ -5,9 +7,7 @@ def create_app(test_config=None):
   app.config.from_mapping(
     SECRET_KEY='super_secret_key'
   )
-
   @app.route('/hello')
   def hello():
-    return 'hello world'
-
+    return 'hello world' 
   return app
